f728df55dc3795dcb8cf4138ea6a7908f7734a09
林孟瑾 <mengjinlin@DN51u4cd.SUNet>
2021-11-12 19:06:20 -0800

    Add SimpleYelp

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# SimpleYelp
-A Yelp clone to practice Kotlin
+# Yelp Clone 
+
+## Meng-Jin Lin
+
+**Yelp clone** displays a list of search results from the Yelp API and displays the results in a scrollable list. 
+
+Time spent: 2 hours spent in total
+
+## Functionality 
+
+The following **required** functionality is completed:
+
+* [x] Ability to query the Yelp API to get results from a search query
+* [x] The search results are displayed in a RecyclerView
+
+The following **extensions** are implemented:
+
+* [x] Changethe theme color to match Yelp
+* [x] Add Yelp logo at the bottom of the app
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='https://media.giphy.com/media/dfUBOZon9R3nt13H9e/giphy.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+## License
+
+    Copyright [2021] [Meng-Jin Lin]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ app/src/main/java/com/example/simpleyelp/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.simpleyelp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val TAG = "MainActivity"
+private const val BASE_URL = "https://api.yelp.com/v3/"
+private const val API_KEY = "KYGzERHgZ0bG0bkHQWq3M9OvdRow_eE7NgsPLepgzLg_mb8P3aDIDnFPXgSj5aKUCLFxDh7uT51IzSzJnnNKLT85q9SRa6aAv4Y2938CBmFpSJzj5IqYcqEuRhqOYXYx"
+class MainActivity : AppCompatActivity() {
+    private lateinit var rvRestaurants: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val restaurants = mutableListOf<YelpRestaurant>()
+        val adapter = RestaurantsAdapter(this, restaurants)
+
+        rvRestaurants = findViewById<RecyclerView>(R.id.rvRestaurants)
+        rvRestaurants.adapter = adapter
+        rvRestaurants.layoutManager = LinearLayoutManager(this)
+
+        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+        val yelpService = retrofit.create(YelpService::class.java)
+        yelpService.searchRestaurants("Bearer $API_KEY","Avocado Toast", "New York").enqueue(object : Callback<YelpSearchResult> {
+            override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
+                Log.i(TAG, "onResponse $response")
+                val body = response.body()
+                if(body == null)
+                {
+                    Log.w(TAG, "did not receive valid response body from Yelp API...exiting")
+                    return
+                }
+                restaurants.addAll(body.restaurants)
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
+                Log.i(TAG, "onFailure $t")
+            }
+
+        })
+
+
+    }
+}
